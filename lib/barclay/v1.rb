120df147f74ce06270897301d03bde670eff2877
Ankur Panchani <ankurmp8990@gmail.com>
2016-09-24 20:17:18 +0530

    Add APIs

--- lib/barclay/v1.rb
+++ lib/barclay/v1.rb
@@ -12,12 +12,55 @@ module Barclay
       #             "subscriber_token" => Settings.barclay_subscriber_token }
       # end
 
-      def customers(query)
-        post('search', { "m" => "autocomplete", "s" => query })
+      def customers()
+        get(Settings.barclay_customer_url, 'customers')
         self
       end
 
+      def customer(customer_id)
+        get(Settings.barclay_customer_url, 'customers/' + customer_id)
+        self
+      end
 
+      def accounts(account_id)
+        get(Settings.barclay_account_url, 'accounts/' + account_id)
+        self
+      end
+
+      def internal_accounts(account_id)
+        get(Settings.barclay_account_url, 'accounts/' + account_id + '/internal-accounts')
+        self
+      end
+
+      def internal_account(account_id, internal_account_id)
+        get(Settings.barclay_account_url, 'accounts/' + account_id + '/internal-accounts/' + internal_account_id)
+        self
+      end
+
+      def payees(account_id)
+        get(Settings.barclay_account_url, '/accounts/' + account_id + '/payees')
+        self
+      end
+
+      def add_payee(account_id, payee)
+        post(Settings.barclay_account_url, '/accounts/' + account_id + '/payees', payee)
+        self
+      end
+
+      def payee(account_id, payee_id)
+        get(Settings.barclay_account_url, '/accounts/' + account_id + '/payees/' + payee_id)
+        self
+      end
+
+      def transactions(account_id)
+        get(Settings.barclay_account_url, '/accounts/' + account_id + '/transactions')
+        self
+      end
+
+      def add_transaction(account_id, transaction)
+        post(Settings.barclay_account_url, '/accounts/' + account_id + '/transactions', transaction)
+        self
+      end
 
       def response
         @response
@@ -28,10 +71,17 @@ module Barclay
       end
 
       private
-        def post(uri, options)
-          uri = URI.parse(Settings.barclay_base_url + uri)
+        def post(url, uri, options)
+          uri = URI.parse(url + uri)
           # options.merge!(self.auth)
           @response = Net::HTTP.post_form(uri, options)
+          self
+        end
+
+        def get(url, uri)
+          uri = URI.parse(url + uri)
+          # options.merge!(self.auth)
+          @response = Net::HTTP.get(uri)
           self
         end
     end
